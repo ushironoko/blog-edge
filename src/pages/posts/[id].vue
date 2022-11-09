d98d4fc43e38e5e2e42d8b813cd3ac1b3c2ab92f
ushironoko <apple19940820@gmail.com>
2022-11-09 23:40:04 +0900

    pending消してみる

--- src/pages/posts/[id].vue
+++ src/pages/posts/[id].vue
@@ -3,15 +3,11 @@ const { params } = useRoute();
 
 const id = Array.isArray(params.id) ? params.id[0] : params.id;
 
-const { data, pending } = useFetch(
-  `/api/posts/postData?id=${encodeURIComponent(id)}`,
-  {
-    key: id,
-  }
-);
+const { data } = useFetch(`/api/posts/postData?id=${encodeURIComponent(id)}`, {
+  key: id,
+});
 </script>
 
 <template>
-  <div v-if="pending"></div>
-  <div v-else v-html="data?.contentHtml"></div>
+  <div v-html="data?.contentHtml"></div>
 </template>
