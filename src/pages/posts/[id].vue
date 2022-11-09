bbdf466ff500af0439225d0628166b84b39b007e
ushironoko <apple19940820@gmail.com>
2022-11-09 23:01:23 +0900

    WIP

--- src/pages/posts/[id].vue
+++ src/pages/posts/[id].vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
 const { params, name } = useRoute();
-const contents = ref('');
 
-if (params.id !== 'url') {
-  const { data } = useFetch(
-    `/api/posts/postData?id=${encodeURIComponent(params.id as string)}`,
-    {
-      key: name?.toString(),
-      initialCache: false,
-    }
-  );
-  contents.value = data.value?.contentHtml || '';
-}
+const { data, pending } = useFetch(
+  `/api/posts/postData?id=${encodeURIComponent(params.id as string)}`,
+  {
+    key: name?.toString(),
+  }
+);
 </script>
 
 <template>
-  <div v-html="contents"></div>
+  <div v-if="pending"></div>
+  <div v-else v-html="data?.contentHtml"></div>
 </template>
