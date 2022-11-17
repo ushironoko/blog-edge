6cadfaaa8de13fa2a66c3365e036d6b028473b1b
ushironoko <apple19940820@gmail.com>
2022-11-17 18:26:48 +0900

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,37 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'ushironoko.me',
+      htmlAttrs: {
+        lang: 'ja',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'ushironokoのブログです。技術・ゲーム・趣味・仕事など。',
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        {
+          name: 'twitter:site',
+          content: '@ushirono_noko',
+        },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/img/ushironoko.jpg',
+        },
+      ],
+    },
+  },
   srcDir: 'src',
   imports: {
     dirs: ['composables/**'],
